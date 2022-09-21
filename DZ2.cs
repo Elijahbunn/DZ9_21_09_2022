46610fd010219a87f3ad68918eafddefc66db7cc
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2022-09-21 20:41:59 +0300

    Add files via upload

---
+++ DZ2.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+PrintSum(N);
+Console.Write($"Sum is: {sum}");
+
+void PrintSum(int N)
+{
+    if (N > M) return;
+    PrintSum(N + 1);
+    sum += N;
+}
