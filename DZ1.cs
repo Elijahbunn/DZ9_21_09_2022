d960558377525a90b3feaf4d88dfa4138eeb6790
Elijah <eligahbunn@gmail.com>
2022-09-21 20:41:17 +0300

    Commit 1

---
+++ DZ1.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+PrintNumber(N);
+
+void PrintNumber(int N)
+{
+    if (N > M) return;
+    Console.Write($"{N} ");
+    PrintNumber(N + 1);
+}
